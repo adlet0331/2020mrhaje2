1a1b2621021e2c408de9687bb28556b46937b8dd
Unreal <scott0202@kaist.ac.kr>
2020-11-23 00:06:31 +0900

    공격 기능 구현

--- Assets/Scripts/UI/SymbolScript.cs
+++ Assets/Scripts/UI/SymbolScript.cs
@@ -2,17 +2,19 @@
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SymbolScript : MonoBehaviour, IPointerClickHandler
+public class SymbolScript : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
     private UIControl pieceUI;
     private GameObject card;
     private SymbolInfo symbolInfo;
+    private TurnManager turnManager;
 
     public enum NameToNum { Infatry, Sniper, MachineGunner }
     public NameToNum cardName;
     public int team;
 
-    // 능력치 보정 값
+    // 능력치
+    public int currentHP;
     public int deltaMove;
     public int deltaRange;
     public int deltaDamage;
@@ -26,27 +28,51 @@ public class SymbolScript : MonoBehaviour, IPointerClickHandler
         pieceUI = GameObject.Find("PieceUI").GetComponent<UIControl>();
         card = pieceUI.transform.Find("Cards").transform.Find("BaseCard").gameObject;
         symbolInfo = GameObject.Find("Symbol Information").GetComponent<SymbolInfo>();
+        turnManager = GameObject.Find("Turn Manager").GetComponent<TurnManager>();
+
+        // HP 초기화
+        currentHP = symbolInfo.cardInfos[(int)cardName].maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // HP 바 조정
+        Vector3 barScale = transform.Find("Current HP Bar").localScale;
+        barScale.x = (float)currentHP / symbolInfo.cardInfos[(int)cardName].maxHP;
+        transform.Find("Current HP Bar").localScale = barScale;
+
+        // HP가 0 이하가 되면 파괴
+        if (currentHP <= 0)
+        {
+            PieceSelect(false);
+            gameObject.SetActive(false);
+        }
     }
 
+    // 선택; 마우스를 뗄 때 실행됨
     public void OnPointerClick(PointerEventData eventData)
     {
+        // 턴이 맞아야만 실행됨
+        if (turnManager.turn != team) return;
         if (!selected)
         {
             // 다른 모든 피스에 대해 PieceSelect(false) 실행
             foreach (SymbolScript symbol in pieceUI.allSymbols) symbol.PieceSelect(false);
-
-            // 턴이 맞아야만 유닛이 선택됨
-            if (GameObject.Find("Turn Manager").GetComponent<TurnManager>().turn == team) PieceSelect(true);
+            PieceSelect(true);
         }
         else
         {
             PieceSelect(false);
+        }
+    }
+
+    // 공격; 유닛을 누르는 순간 실행됨
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (pieceUI.attackSelected)
+        {
+            pieceUI.selectedPiece.GetComponent<SymbolScript>().Attack(this);
         }
     }
 
@@ -59,17 +85,15 @@ public class SymbolScript : MonoBehaviour, IPointerClickHandler
             pieceUI.selectedPiece = gameObject;
 
             // card 관련 처리
-            {
-                card.transform.Find("Card Image").GetComponent<Image>().sprite = symbolInfo.cardInfos[(int)cardName].cardImage;
-                card.transform.Find("Name").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].cardName;
-                card.transform.Find("MaxHP").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].maxHP.ToString();
-                card.transform.Find("Ability Name").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].abilityName;
-                card.transform.Find("Ability Information").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].abilityInformation;
-                card.transform.Find("Move").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].move.ToString() + "+" + deltaMove.ToString();
-                card.transform.Find("Range").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].range.ToString() + "+" + deltaRange.ToString();
-                card.transform.Find("Damage").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].damage.ToString() + "+" + deltaDamage.ToString();
-                card.SetActive(true);
-            }
+            card.transform.Find("Card Image").GetComponent<Image>().sprite = symbolInfo.cardInfos[(int)cardName].cardImage;
+            card.transform.Find("Name").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].cardName;
+            card.transform.Find("MaxHP").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].maxHP.ToString();
+            card.transform.Find("Ability Name").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].abilityName;
+            card.transform.Find("Ability Information").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].abilityInformation;
+            card.transform.Find("Move").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].move.ToString() + "+" + deltaMove.ToString();
+            card.transform.Find("Range").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].range.ToString() + "+" + deltaRange.ToString();
+            card.transform.Find("Damage").GetComponent<Text>().text = symbolInfo.cardInfos[(int)cardName].damage.ToString() + "+" + deltaDamage.ToString();
+            card.SetActive(true);
 
             pieceUI.transform.Find("Move Button").gameObject.SetActive(true);
             pieceUI.transform.Find("Attack Button").gameObject.SetActive(true);
@@ -92,11 +116,22 @@ public class SymbolScript : MonoBehaviour, IPointerClickHandler
 
     public void Move(GameBoard board)
     {
-        Debug.Log("Move");
         if (Mathf.Abs(transform.parent.GetComponent<GameBoard>().index - board.index) <= symbolInfo.cardInfos[(int)cardName].move + deltaMove)
         {
             transform.SetParent(board.transform);
         }
         pieceUI.moveSelected = false;
+        PieceSelect(false);
+    }
+
+    public void Attack(SymbolScript target)
+    {
+        if (Mathf.Abs(transform.parent.GetComponent<GameBoard>().index - target.transform.parent.GetComponent<GameBoard>().index)
+            <= symbolInfo.cardInfos[(int)cardName].range + deltaRange)
+        {
+            target.currentHP -= symbolInfo.cardInfos[(int)cardName].damage + deltaDamage;
+        }
+        pieceUI.attackSelected = false;
+        PieceSelect(false);
     }
 }
